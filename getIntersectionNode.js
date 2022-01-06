06ffb9fd855e50ef09f6035218afebf138406d43
swift <swift@163.com>
2022-01-06 09:00:53 +0800

    getIntersectionNode

---
+++ getIntersectionNode.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+// var getIntersectionNode = function(headA, headB) {
+//     let set = new Set();
+//     let p1 = headA, p2 = headB;
+//     while(p1){
+//         set.add(p1);
+//         p1 = p1.next;
+//     }
+
+//     while(p2){
+//         if(set.has(p2)){
+//             return p2;
+//         }
+//         p2 = p2.next;
+//     }
+//     return null
+// };
+
+var getIntersectionNode = function(headA, headB) {
+    let p1 = headA, p2 = headB;
+    while(p1 != p2 ){
+        p1 = p1 === null ? headB : p1.next;
+        p2 = p2 === null ? headA : p2.next;
+    }
+    return p1 == p2 ? p1 : null;
+};
