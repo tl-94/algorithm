a0e0f3191ee0750273bcd697fd459e727cc05533
tl-94 <1982418781@qq.com>
2022-01-06 09:16:25 +0800

    getIntersectionNode

--- getIntersectionNode.js
+++ getIntersectionNode.js
@@ -11,22 +11,22 @@
  * @param {ListNode} headB
  * @return {ListNode}
  */
-// var getIntersectionNode = function(headA, headB) {
-//     let set = new Set();
-//     let p1 = headA, p2 = headB;
-//     while(p1){
-//         set.add(p1);
-//         p1 = p1.next;
-//     }
+var getIntersectionNode = function(headA, headB) {
+    let set = new Set();
+    let p1 = headA, p2 = headB;
+    while(p1){
+        set.add(p1);
+        p1 = p1.next;
+    }
 
-//     while(p2){
-//         if(set.has(p2)){
-//             return p2;
-//         }
-//         p2 = p2.next;
-//     }
-//     return null
-// };
+    while(p2){
+        if(set.has(p2)){
+            return p2;
+        }
+        p2 = p2.next;
+    }
+    return null
+};
 
 var getIntersectionNode = function(headA, headB) {
     let p1 = headA, p2 = headB;
